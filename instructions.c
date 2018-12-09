1d365aa4f5a2088a73b04b0892b02fadf8c104d7
landhb <landherrb@gmail.com>
2018-12-09 11:53:16 -0600

    starting instructions + tests for them

---
+++ instructions.c
@@ -0,0 +1,65 @@
+#include "vm.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int parse_file(FILE * f, registers * reg, stack_info * stack) {
+	
+	int i;
+	char * buffer;
+	unsigned long file_len;
+	
+	// get filesize
+	fseek(f, 0, SEEK_END);
+	file_len=ftell(f);
+	fseek(f, 0, SEEK_SET);
+	if(file_len <= 0) {
+		return -1;
+	}
+
+	// create memory space for the "process"
+	buffer = malloc(file_len);
+	if(!buffer) {
+		return -1;
+	}
+
+	// read the entire file into memory
+	fread(buffer, file_len, 1, f);
+	fclose(f);
+
+	// step through instructions
+	i=0;
+	while(i < file_len) {
+		i+=run_instruction(buffer, i, reg, stack);	
+	}
+
+	return 0;
+}
+
+
+
+int run_instruction(char * buffer, int i, registers * reg, stack_info * stack) {
+	switch(GET_RIGHTMOST_16_BITS(buffer[i])) {
+		
+		case 0: //halt
+			exit(0);
+		
+		case 1: // set register <a> to <b>
+			set_register(reg, buffer[i+REG_SIZE_BYTES], &(buffer[i+2*REG_SIZE_BYTES]), REG_SIZE_BYTES);
+			return REG_SIZE_BYTES*3;
+		
+		case 2: // push <a>
+			if(buffer[i+REG_SIZE_BYTES] < 32767) {
+				// push memory
+			}
+			else if (buffer[i+REG_SIZE_BYTES] <= 32775) {
+				// push register	
+				push_stack(stack, reg, buffer[i+REG_SIZE_BYTES]-32768);
+			} 
+			// TODO? exit on invalid address/register
+			return REG_SIZE_BYTES*2;
+		// invalid instruction
+		default:
+			exit(-1);
+
+	}
+}
--- tests/test.c
+++ tests/test.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> // fork, getpid, exec
+#include <sys/types.h> //pid_t datatype
+#include <sys/wait.h> //waitpid()
 
 describe(registers) {
 	it("write/read registers") {
@@ -14,14 +17,14 @@ describe(registers) {
 		// set registers
 		asserteq(set_register(reg, 1, "dead", strlen("dead")),0);
 		asserteq(set_register(reg, 2, "beef", strlen("beef")),0);
-		
+
 		// verify first
 		asserteq(get_register(reg, 1, buf), 0);
-		asserteq_buf(buf, "dead", REG_SIZE/8);
-
+		asserteq_buf(buf, "dead", REG_SIZE_BYTES);
+		
 		// verify second	
 		asserteq(get_register(reg, 2, buf), 0);
-		asserteq_buf(buf, "beef", REG_SIZE/8); 
+		asserteq_buf(buf, "beef", REG_SIZE_BYTES); 
 		defer(free(reg));
 		defer(free(buf));
 	}
@@ -39,72 +42,157 @@ describe(stack) {
 	subdesc(stack_functions) {
 		it("push onto stack") {
 			stack_info * stack = stack_init(INIT_STACK_SIZE);
+			registers * reg = malloc(sizeof(struct registers));
 			assertneq(stack, NULL);
 	
 			// test data
 			char * tmp;
 			tmp = calloc(1,REG_SIZE);
-			memcpy(tmp, "12345678912345600", REG_SIZE-1);
-		
+			memcpy(tmp, "12345678912345600", REG_SIZE_BYTES);
+			set_register(reg, 0, tmp, REG_SIZE_BYTES);
+
 			// push onto stack
-			asserteq(push_stack(stack,tmp),0);
-			asserteq_buf(stack->mem, tmp, REG_SIZE);
+			asserteq(push_stack(stack,reg, 0),0);
+			asserteq_buf(stack->mem, tmp, REG_SIZE_BYTES);
 			defer(cleanup_stack(stack));
+			defer(free(reg));
 			defer(free(tmp));
 		}
 
 		it("pop off stack") {
 			stack_info * stack = stack_init(INIT_STACK_SIZE);
+			registers * reg = malloc(sizeof(struct registers));
 			assertneq(stack, NULL);
 	
 			// test data
-			char * tmp;
+			char * tmp, *res;
 			tmp = calloc(1,REG_SIZE);
+			res = calloc(1,REG_SIZE);
 			memcpy(tmp, "12345678912345600", REG_SIZE-1);
-		
-			// push onto stack
-			asserteq(push_stack(stack,tmp),0);
-			asserteq_buf(stack->mem, tmp, REG_SIZE);
+			set_register(reg, 0, tmp, REG_SIZE_BYTES);
+			
+			// push tmp onto stack
+			asserteq(push_stack(stack, reg, 0),0);
+			asserteq_buf(stack->mem, tmp, REG_SIZE_BYTES);
 
-			// clear buff
-			bzero(tmp, REG_SIZE);
-			assertneq_buf(stack->mem, tmp, REG_SIZE);
-
-			// pop off stack
-			pop_stack(stack, tmp);
-			asserteq_buf(stack->mem, tmp, REG_SIZE);
+			// pop off stack into res
+			pop_stack(stack, reg, 1);  // pop stack item into r1
+			get_register(reg, 1, res); // place r1 into res
+			asserteq_buf(res, tmp, REG_SIZE_BYTES); //cmp res to tmp
 			defer(cleanup_stack(stack));
+			defer(free(reg));
 			defer(free(tmp));
+			defer(free(res));
 		}
 		it("stack resizes") {
 			stack_info * stack = stack_init(INIT_STACK_SIZE);
+			registers * reg = malloc(sizeof(struct registers));
 			assertneq(stack, NULL);
 	
 			// test data
-			char * tmp;
+			char * tmp, *res;
 			tmp = calloc(1,REG_SIZE);
+			res = calloc(1,REG_SIZE);
 			memcpy(tmp, "12345678912345600", REG_SIZE-1);
 		
 			// push > INIT_STACK_SIZE data onto stack
-			while(stack->total_size <= INIT_STACK_SIZE + REG_SIZE*STACK_RESIZE*2) {
-				asserteq(push_stack(stack,tmp), 0);
-				asserteq_buf(stack->mem, tmp, REG_SIZE);
+			while(stack->total_size <= INIT_STACK_SIZE + REG_SIZE_BYTES*STACK_RESIZE*2) {
+				set_register(reg, 0, tmp, REG_SIZE_BYTES);
+				asserteq(push_stack(stack,reg,0), 0);
+				asserteq_buf(stack->mem, tmp, REG_SIZE_BYTES);
 			}
 
 			// clear stack
 			while(stack->num_elements > 0) {
-				// clear buff
-				bzero(tmp, REG_SIZE);
-				assertneq_buf(stack->mem, tmp, REG_SIZE);
 
 				// pop off stack
-				pop_stack(stack, tmp);
-				asserteq_buf(stack->mem, tmp, REG_SIZE);
+				pop_stack(stack, reg, 1);
+				get_register(reg, 1, res);
+				asserteq_buf(res, tmp, REG_SIZE_BYTES);
 			}
 			defer(cleanup_stack(stack));
-			defer(free(tmp));	
+			defer(free(reg));
+			defer(free(tmp));
+			defer(free(res));	
 		}
 	}
+}
+
+describe(instructions) {
+	it("halt") {
+		stack_info * stack = stack_init(INIT_STACK_SIZE);
+		registers * reg = malloc(sizeof(struct registers));
+		assertneq(stack, NULL);
+
+		// test instruction
+		char *buf;
+		buf = malloc(4);
+		memcpy(buf, "\x00\x00\x01\x02", 4);
+
+		// fork child to test halt() instruction
+		pid_t pid = fork();
+		int status= -1;
+		
+		// child will run the instruction
+		if(pid==0) {
+			run_instruction(buf, 0, reg, stack);	
+		}
+		else {
+			// assert that child exited properly
+			waitpid(pid, &status, 0);
+			asserteq(WIFEXITED(status),1);
+		}
+		defer(free(buf));
+		defer(cleanup_stack(stack));
+		defer(free(reg));
+	}
+	it("set") {
+		stack_info * stack = stack_init(INIT_STACK_SIZE);
+		registers * reg = malloc(sizeof(struct registers));
+		assertneq(stack, NULL);
+
+		// test instruction
+		char *buf,*res;
+		buf = malloc(6);
+		res = malloc(REG_SIZE_BYTES);
+		memcpy(buf, "\x01\x00\x01\x00\x04\x00", 6);
+		
+		// should return 6
+		asserteq(run_instruction(buf, 0, reg, stack),6);	
+		
+		// test if register was set
+		asserteq(get_register(reg, 1, res), 0);
+		asserteq_buf(res, "\x04\x00", REG_SIZE_BYTES);
+		
+		defer(free(buf));
+		defer(cleanup_stack(stack));
+		defer(free(reg));	
+		defer(free(res));
+	}
+	it("push") {
+		stack_info * stack = stack_init(INIT_STACK_SIZE);
+		registers * reg = malloc(sizeof(struct registers));
+		assertneq(stack, NULL);
+
+		// test instruction
+		char *buf,*res;
+		buf = malloc(6);
+		res = malloc(REG_SIZE_BYTES);
+		memcpy(buf, "\x02\x00\x01\x00\x04\x00", 6);
+		
+		// should return 4
+		asserteq(run_instruction(buf, 0, reg, stack),4);	
+		
+		// test if register was set
+		asserteq(get_register(reg, 1, res), 0);
+		asserteq_buf(res, "\x04\x00", REG_SIZE_BYTES);
+		
+		defer(free(buf));
+		defer(cleanup_stack(stack));
+		defer(free(reg));	
+		defer(free(res));	
+	}
+
 }
 
 snow_main();
